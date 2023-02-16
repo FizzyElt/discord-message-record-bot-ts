860c89cd943a79b1607535f94e12162ca9aa1675
FizzyElt <fizzyelt8786@gmail.com>
2023-02-16 16:02:01 +0800

    refactor check user function

--- tasks/check_banned_user.ts
+++ tasks/check_banned_user.ts
@@ -1,23 +1,28 @@
 import { Message } from 'discord.js';
 import * as O from 'fp-ts/Option';
 import * as TO from 'fp-ts/TaskOption';
+import * as T from 'fp-ts/Task';
+import * as IO from 'fp-ts/IO';
+import * as IOOption from 'fp-ts/IOOption';
 import { pipe, flow } from 'fp-ts/function';
 import { format } from 'date-fns/fp';
 import { isAfter } from 'date-fns';
-import * as R from 'ramda';
 import userBlackList from '../store/user_black_list';
 
 function checkBannedUser(msg: Message<boolean>): TO.TaskOption<Message<boolean>> {
   return pipe(
     userBlackList.getBanedUser(msg.author.id),
-    O.filter((date) =>
+    IOOption.chainFirst((date) =>
       pipe(
         isAfter(Date.now(), date),
-        R.tap((isExpired) => isExpired && userBlackList.removeUser(msg.author.id)),
-        R.not
+        IO.of,
+        IO.chainFirst((isExpired) =>
+          isExpired ? userBlackList.removeUser(msg.author.id) : IO.of(isExpired)
+        ),
+        IO.map((isExpired) => (isExpired ? O.none : O.some(isExpired)))
       )
     ),
-    TO.fromOption,
+    T.fromIO,
     TO.chainFirst(() => TO.tryCatch(() => msg.delete())),
     TO.chain(
       flow(
